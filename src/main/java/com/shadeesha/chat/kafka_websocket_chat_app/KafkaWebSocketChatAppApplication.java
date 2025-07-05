132de4be97333bbc592ae3ad03b52518655c0c41
shadeesha <hshadeesha@gmail.com>
2025-07-05 22:38:07 +0100

    initial commit

---
+++ src/main/java/com/shadeesha/chat/kafka_websocket_chat_app/KafkaWebSocketChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.shadeesha.chat.kafka_websocket_chat_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaWebSocketChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaWebSocketChatAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shadeesha/chat/kafka_websocket_chat_app/KafkaWebSocketChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shadeesha.chat.kafka_websocket_chat_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaWebSocketChatAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
